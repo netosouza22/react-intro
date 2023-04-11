e18d601232fc16a5d631a9a7b6ac4e8b71d2b248
= <=>
2023-04-11 16:47:21 -0300

    refactor: mudando exemplo de useEffect

--- src/pages/usandoeffect/index.tsx
+++ src/pages/usandoeffect/index.tsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from "react";
 
-export default function UsingEffect() {
-  const [counter, setCounter] = useState<number>(0);
-  const [message, setMessage] = useState<string>("");
+interface IGithubData {
+  name: string;
+  bio: string;
+  html_url: string;
+}
+export default function GitHubData() {
+  const [userData, setUserData] = useState<IGithubData | null>(null);
 
   useEffect(() => {
-    if (Math.sign(counter) === -1) {
-      setMessage("Número Negativo");
-    } else {
-      setMessage("Número positivo ou zero");
-    }
-  }, [counter]);
+    fetch("https://api.github.com/users/netosouza22")
+      .then((response) => response.json())
+      .then((data) => setUserData(data));
+  }, []);
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h1>{message}</h1>
-      <span>{counter}</span>
-      <button onClick={() => setCounter((state) => state + 1)}>Aumentar</button>
-      <button onClick={() => setCounter((state) => state - 1)}>Diminuir</button>
+      <h1>{userData.name}</h1>
+      <p>{userData.bio}</p>
+      <a href={userData.html_url}>View Profile</a>
     </div>
   );
 }
